0aadfa1ccf499a55450da27fb0233503c8b41618
Tao-Sheng <taosheng.chen@gmail.com>
2017-07-07 09:24:20 +0000

    a register sensor to wait for Shadow changes and a test shadow change client

---
+++ AirSensor/src/aws_air_shadow_trigger.py
@@ -0,0 +1,118 @@
+
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTShadowClient
+import sys
+import logging
+import time
+import json
+import argparse
+
+
+
+# Shadow JSON schema:
+#
+# Name: Bot
+# {
+#	"state": {
+#		"desired":{
+#			"property":<INT VALUE>
+#		}
+#	}
+# }
+
+def customShadowCallback_Get(payload, responseStatus, token):
+    print(payload)
+
+# Custom Shadow callback
+def customShadowCallback_Update(payload, responseStatus, token):
+    # payload is a JSON string ready to be parsed using json.loads(...)
+    # in both Py2.x and Py3.x
+    if responseStatus == "timeout":
+        print("Update request " + token + " time out!")
+    if responseStatus == "accepted":
+        payloadDict = json.loads(payload)
+        print("Updated" + str(payloadDict))
+        print("~~~~~~~~~~~~~~~~~~~~~~~")
+    if responseStatus == "rejected":
+        print("Update request " + token + " rejected!")
+
+def customShadowCallback_Delete(payload, responseStatus, token):
+    if responseStatus == "timeout":
+        print("Delete request " + token + " time out!")
+    if responseStatus == "accepted":
+        print("~~~~~~~~~~~~~~~~~~~~~~~")
+        print("Delete request with token: " + token + " accepted!")
+        print("~~~~~~~~~~~~~~~~~~~~~~~\n\n")
+    if responseStatus == "rejected":
+        print("Delete request " + token + " rejected!")
+
+# Read in command-line parameters
+parser = argparse.ArgumentParser()
+parser.add_argument("-e", "--endpoint", action="store", required=True, dest="host", help="Your AWS IoT custom endpoint")
+parser.add_argument("-r", "--rootCA", action="store", required=True, dest="rootCAPath", help="Root CA file path")
+parser.add_argument("-c", "--cert", action="store", dest="certificatePath", help="Certificate file path")
+parser.add_argument("-k", "--key", action="store", dest="privateKeyPath", help="Private key file path")
+parser.add_argument("-w", "--websocket", action="store_true", dest="useWebsocket", default=False,
+                    help="Use MQTT over WebSocket")
+parser.add_argument("-n", "--thingName", action="store", dest="thingName", default="Bot", help="Targeted thing name")
+parser.add_argument("-id", "--clientId", action="store", dest="clientId", default="basicShadowUpdater", help="Targeted client id")
+
+args = parser.parse_args()
+host = args.host
+rootCAPath = args.rootCAPath
+certificatePath = args.certificatePath
+privateKeyPath = args.privateKeyPath
+useWebsocket = args.useWebsocket
+thingName = args.thingName
+clientId = args.clientId
+
+if args.useWebsocket and args.certificatePath and args.privateKeyPath:
+    parser.error("X.509 cert authentication and WebSocket are mutual exclusive. Please pick one.")
+    exit(2)
+
+if not args.useWebsocket and (not args.certificatePath or not args.privateKeyPath):
+    parser.error("Missing credentials for authentication.")
+    exit(2)
+
+# Configure logging
+logger = logging.getLogger("AWSIoTPythonSDK.core")
+logger.setLevel(logging.ERROR)
+streamHandler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+streamHandler.setFormatter(formatter)
+logger.addHandler(streamHandler)
+
+# Init AWSIoTMQTTShadowClient
+myAWSIoTMQTTShadowClient = None
+if useWebsocket:
+    myAWSIoTMQTTShadowClient = AWSIoTMQTTShadowClient(clientId, useWebsocket=True)
+    myAWSIoTMQTTShadowClient.configureEndpoint(host, 443)
+    myAWSIoTMQTTShadowClient.configureCredentials(rootCAPath)
+else:
+    myAWSIoTMQTTShadowClient = AWSIoTMQTTShadowClient(clientId)
+    myAWSIoTMQTTShadowClient.configureEndpoint(host, 8883)
+    myAWSIoTMQTTShadowClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
+
+# AWSIoTMQTTShadowClient configuration
+myAWSIoTMQTTShadowClient.configureAutoReconnectBackoffTime(1, 32, 20)
+myAWSIoTMQTTShadowClient.configureConnectDisconnectTimeout(10)  # 10 sec
+myAWSIoTMQTTShadowClient.configureMQTTOperationTimeout(5)  # 5 sec
+
+# Connect to AWS IoT
+myAWSIoTMQTTShadowClient.connect()
+
+# Create a deviceShadow with persistent subscription
+deviceShadowHandler = myAWSIoTMQTTShadowClient.createShadowHandlerWithName(thingName, True)
+
+
+print("---- shadowGet ----")
+deviceShadowHandler.shadowGet(customShadowCallback_Get, 5)
+
+JSONPayload = '{"state":{"desired":{ "action":"do"}}}'
+print("---- shadow update to trigger air sensor ----")
+deviceShadowHandler.shadowUpdate(JSONPayload, customShadowCallback_Update, 5)
+time.sleep(2)
+deviceShadowHandler.shadowGet(customShadowCallback_Get, 5)
+time.sleep(2)
+myAWSIoTMQTTShadowClient.disconnect()
+
+
